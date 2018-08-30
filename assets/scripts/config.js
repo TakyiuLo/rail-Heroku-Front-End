e1e87fb18fcad4e0a5c3aafe1f9c6f1bf12fa6fc
Takyiu Lo <takyiulo@gmail.com>
2018-08-30 15:19:54 -0400

    Added heroku domain to config for production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://rail-heroku-project-temple.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
